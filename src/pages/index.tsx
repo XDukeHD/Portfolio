6fce196b16c95757bb39ae4a4a1855d0ea21dc21
Tulio Cadilhac <tuliocadilhaczanella@gmail.com>
2024-03-15 00:01:39 -0300

    esqueci de uma coisa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Index() {
         <title>xDuke</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width, shrink-to-fit=no" />
         <meta name="language" content="EN-US" />
-        <meta name="author" content="XDukeHD" />
+        <meta name="author" content="XDuke" />
         <meta name="robots" content="index,follow,noodp,noydir" />
         <meta name="revisit-after" content="1 week" />
         <link rel="icon" href='/logo.ico' type="x-icon/icon" />
@@ -25,27 +25,27 @@ export default function Index() {
           name="keywords"
           content="Buy discord bot, duke, xduke, xdukehd, xduke_br, tulio, tulio_zanella, tuliozanella, tuliozanella.com, tul, mr duke, mrduke, mrdukehd, mrduke_br"
         />
-        <meta property="og:url" content="https://xduke.tech/" />
+        <meta property="og:url" content="https://xduke.dev/" />
         <meta
           property="og:title"
-          content="Welcome to my portfolio, view my projects and contact me."
+          content="Hi! i'm XDuke!"
         />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="pt_br" />
         <meta
           property="og:site_name"
-          content="Welcome to my portfolio, view my projects and contact me."
+          content="XDuke"
         />
         <meta property="og:image" content="../public/img/chocolatebear.png" />
         <meta
           property="og:description"
-          content="Contact me for buy applications."
+          content="This is my portfolio, feel free to browse some information about me and my projects."
         />
         <meta
           property="og:keywords"
           content="duke, xduke, xdukehd, xduke_br, tulio, tulio_zanella, tuliozanella, tuliozanella.com, tul, mr duke, mrduke, mrdukehd, mrduke_br,"
         />
-        <meta name="description" content="Contact me for buy applications." />
+        <meta name="description" content="This is my portfolio, feel free to browse some information about me and my projects." />
         <script src="../styles/bootstrap.min.js"/>
       </Head>
       <Navbar />
